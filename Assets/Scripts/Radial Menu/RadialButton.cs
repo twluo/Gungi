8536474bf1a24a935fcba316c11e15e28ea413f3
Tony Luo <tonywluo@gmail.com>
2016-07-09 12:28:00 -0500

    Changed name of an asset

--- Assets/Scripts/Radial Menu/RadialButton.cs
+++ Assets/Scripts/Radial Menu/RadialButton.cs
@@ -30,6 +30,7 @@ public class RadialButton : MonoBehaviour {
         newPiece.GetComponent<Piece>().location = tile;
         newPiece.transform.position = tile.transform.position + new Vector3(0f, piece.transform.localScale.y/2 + piece.transform.localScale.y * tile.numberPieces, 0f);
         tile.numberPieces++;
+        closeMenu();
     }
 
     public void closeMenu() {
